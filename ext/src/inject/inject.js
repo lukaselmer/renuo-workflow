d0ffa294f6f6426d5e952f766899c8e966f36f47
Lukas Elmer <lukas.elmer@gmail.com>
2015-03-16 00:15:58 +0100

    filter and focus

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -22,7 +22,7 @@ $(function(){
   function initRedmineHomePage(){
     var left = $(".splitcontentleft");
     var leftInner = $("<div></div").addClass("projects box").appendTo(left);
-    $("<h3>All projects</h3>").appendTo(leftInner);
+    $("<h3>All projects </h3>").append($("<input>").attr("id", "quicksearch")).appendTo(leftInner);
     var list = $("<ul></ul>");
 
     $("#project_quick_jump_box option").each(function(_index, value){
@@ -31,9 +31,22 @@ $(function(){
       var link = $("<a></a>").text(project).attr("href", url);
       
       if(url && url !== ""){
-        $("<li></li>").append(link).appendTo(list);
+        $("<li></li>").addClass("project-filterable").append(link).appendTo(list);
       }
     });
     leftInner.append(list);
+    
+    $("#quicksearch").bind("keyup change input", function(){
+      var search = $("#quicksearch").val().toLowerCase();
+      $(".project-filterable").each(function(_id, value){
+        if($(value).text().toLowerCase().indexOf(search) == -1){
+          $(value).hide();
+        } else{
+          $(value).show();
+        }
+      });
+    });
+    
+    $("#quicksearch").focus();
   }
 });
