c5d8d803d4fcf6f7e2ceb018f4718033f51c46fa
Lukas Elmer <lukas.elmer@gmail.com>
2015-03-16 00:03:06 +0100

    redmine home page

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,13 +1,39 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+"use strict";
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+$(function(){
+  init();
 
-	}
-	}, 10);
+  function init(){
+    if(isRedmineHomePage()) initRedmineHomePage();
+  }
+
+  function getBrowserDomain(){
+    return window.location.href.split("/")[2];
+  }
+  
+  function getBrowserUrl(){
+    return window.location.href.split("/")[3];
+  }
+  
+  function isRedmineHomePage(){
+    return getBrowserDomain() === "redmine.renuo.ch" && getBrowserUrl() === "";
+  }
+  
+  function initRedmineHomePage(){
+    var left = $(".splitcontentleft");
+    var leftInner = $("<div></div").addClass("projects box").appendTo(left);
+    $("<h3>All projects</h3>").appendTo(leftInner);
+    var list = $("<ul></ul>");
+
+    $("#project_quick_jump_box option").each(function(_index, value){
+      var url = $(value).val();
+      var project = $(value).text();
+      var link = $("<a></a>").text(project).attr("href", url);
+      
+      if(url && url !== ""){
+        $("<li></li>").append(link).appendTo(list);
+      }
+    });
+    leftInner.append(list);
+  }
 });
