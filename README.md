6d36d2f772522c727e395908866f2b8d42b0d9b8
Lukas Elmer <lukas.elmer@gmail.com>
2015-03-20 01:15:58 +0100

    packing

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Use ```grunt serve``` for development.
 ## Deployment
 
 * Download the app.pem from here: https://redmine.renuo.ch/attachments/download/1926/app.pem
+* Increase the version number in manifest.json.
 * Use pack.sh to pack the extension.
 * Upload it to here: https://chrome.google.com/webstore/developer/dashboard
--- pack.sh
+++ pack.sh
@@ -1,1 +1,9 @@
-/opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=app --pack-extension-key=app.pem
+cp app.pem app/key.pem
+#/opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=app --pack-extension-key=app.pem
+echo "Zip the dir ./app. I will open a finder for you. Hit [Enter] when you're ready"
+read
+open .
+echo "Ok, ready when you are"
+read
+rm app/key.pem
+
