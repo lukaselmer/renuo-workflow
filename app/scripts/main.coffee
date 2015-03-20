9cf4ed7e17ac42ed15f82aaf538a539f91117048
Lukas Elmer <lukas.elmer@gmail.com>
2015-03-20 01:19:51 +0100

    v2

--- app/scripts/main.coffee
+++ app/scripts/main.coffee
@@ -1,5 +1,5 @@
 'use strict'
 
 $ ->
-  window.defineStore.loadDependency('redmine/home')
   window.defineStore.loadDependency('redmine/global')
+  window.defineStore.loadDependency('redmine/home')
--- pack.sh
+++ pack.sh
@@ -1,9 +1,9 @@
 cp app.pem app/key.pem
 #/opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=app --pack-extension-key=app.pem
-echo "Zip the dir ./app. I will open a finder for you. Hit [Enter] when you're ready"
-read
+zip -r app.zip app
+echo "please upload the app.zip to https://chrome.google.com/webstore/developer/dashboard"
+echo "hit [Enter] to open the finder and the dashboard"
 open .
-echo "Ok, ready when you are"
-read
+open https://chrome.google.com/webstore/developer/dashboard
 rm app/key.pem
 
