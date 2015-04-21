924e35eedb47c65fc9dc19b4fccfcda139c01e7c
Lukas Elmer <lukas.elmer@gmail.com>
2015-04-22 00:08:59 +0200

    reformat code

--- app/scripts/helpers/hotkeyManager.ts
+++ app/scripts/helpers/hotkeyManager.ts
@@ -1,34 +1,34 @@
 ///<reference path="../typings/all.d.ts"/>
 
 class HotkeyManager {
-  hotkeys = [];
+    hotkeys = [];
 
-  constructor() {
-    this.hotkeys = [];
-    this.registerHotkey("Show help", "?", () => this.displayHelp());
-    this.registerHotkey("Close help", "esc", () => this.hideHelp());
-  }
+    constructor() {
+        this.hotkeys = [];
+        this.registerHotkey("Show help", "?", () => this.displayHelp());
+        this.registerHotkey("Close help", "esc", () => this.hideHelp());
+    }
 
-  // more info for the keyCombination: http://craig.is/killing/mice
-  public registerHotkey(name, keyCombination, func) {
-    this.hotkeys.push([keyCombination, name]);
-    return Mousetrap.bind(keyCombination, func);
-  }
+    // more info for the keyCombination: http://craig.is/killing/mice
+    public registerHotkey(name, keyCombination, func) {
+        this.hotkeys.push([keyCombination, name]);
+        return Mousetrap.bind(keyCombination, func);
+    }
 
-  public displayHelp() {
-    var inner, keyCombination, modal, name;
-    inner = $("<div></div>").addClass("hotkey-modal-inner");
-    this.hotkeys.forEach((hotkey) => {
-      keyCombination = hotkey[0], name = hotkey[1];
-      return $("<div></div>").append($("<div></div>").addClass("key-combination").text(keyCombination)).append($("<div></div>").addClass("name").text(name)).append($("<div></div>").addClass("clear-both")).appendTo(inner);
-    });
+    public displayHelp() {
+        var inner, keyCombination, modal, name;
+        inner = $("<div></div>").addClass("hotkey-modal-inner");
+        this.hotkeys.forEach((hotkey) => {
+            keyCombination = hotkey[0], name = hotkey[1];
+            return $("<div></div>").append($("<div></div>").addClass("key-combination").text(keyCombination)).append($("<div></div>").addClass("name").text(name)).append($("<div></div>").addClass("clear-both")).appendTo(inner);
+        });
 
-    return modal = $("<div></div>").addClass("hotkey-modal").append(inner).prependTo($("body"));
-  }
+        return modal = $("<div></div>").addClass("hotkey-modal").append(inner).prependTo($("body"));
+    }
 
-  public hideHelp() {
-    return $(".hotkey-modal").remove();
-  }
+    public hideHelp() {
+        return $(".hotkey-modal").remove();
+    }
 }
 
 window.define("helpers/hotkeyManager", [], () => new HotkeyManager());
--- app/scripts/helpers/urlHelper.ts
+++ app/scripts/helpers/urlHelper.ts
@@ -1,31 +1,31 @@
 ///<reference path="../typings/all.d.ts"/>
 
 class UrlHelper {
-  getBrowserDomain() {
-    console.log('yep, working!')
-    return window.location.href.split('/')[2]
-  }
+    getBrowserDomain() {
+        console.log('yep, working!')
+        return window.location.href.split('/')[2]
+    }
 
-  getBrowserUrlRaw() {
-    return window.location.href.split('/').slice(3, -1)
-  }
+    getBrowserUrlRaw() {
+        return window.location.href.split('/').slice(3, -1)
+    }
 
-  getBrowserUrl() {
-    return this.getBrowserUrlRaw().join('/')
-  }
+    getBrowserUrl() {
+        return this.getBrowserUrlRaw().join('/')
+    }
 
-  isRedmineHomePage() {
-    return this.isRedmine() && this.getBrowserUrl() == ''
-  }
+    isRedmineHomePage() {
+        return this.isRedmine() && this.getBrowserUrl() == ''
+    }
 
-  isRedmineIssuesPage() {
-    var url = this.getBrowserUrl()
-    return this.isRedmine() && (url.includes('issues?') || url.endsWith('issues'))
-  }
+    isRedmineIssuesPage() {
+        var url = this.getBrowserUrl()
+        return this.isRedmine() && (url.includes('issues?') || url.endsWith('issues'))
+    }
 
-  isRedmine() {
-    return this.getBrowserDomain() == 'redmine.renuo.ch'
-  }
+    isRedmine() {
+        return this.getBrowserDomain() == 'redmine.renuo.ch'
+    }
 }
 
 window.define('helpers/urlHelper', [], () => new UrlHelper())
